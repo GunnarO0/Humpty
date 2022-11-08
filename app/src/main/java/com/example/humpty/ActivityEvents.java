065c1389ac0b788aaccd289a2668fd84f46f5175
Gunnar Ortega <gorteg6@lsu.edu>
2022-11-07 22:11:48 -0600

    navigation bar and three pages

--- app/src/main/java/com/example/humpty/ActivityEvents.java
+++ app/src/main/java/com/example/humpty/ActivityEvents.java
@@ -2,29 +2,25 @@ package com.example.humpty;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.ActionBarOverlayLayout;
-import androidx.navigation.fragment.NavHostFragment;
 
-import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class ActivityHome extends AppCompatActivity {
+public class ActivityEvents extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_events);
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
 
         // Set Home selected
-        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setSelectedItemId(R.id.events);
 
         // Perform item selected listener
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -33,11 +29,11 @@ public class ActivityHome extends AppCompatActivity {
 
                 switch(item.getItemId())
                 {
-                    case R.id.events:
-                        startActivity(new Intent(getApplicationContext(),ActivityEvents.class));
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),ActivityHome.class));
                         overridePendingTransition(0,0);
                         return true;
-                    case R.id.home:
+                    case R.id.events:
                         return true;
                     case R.id.social:
                         startActivity(new Intent(getApplicationContext(),ActivitySocial.class));
@@ -47,6 +43,5 @@ public class ActivityHome extends AppCompatActivity {
                 return false;
             }
         });
-
     }
 }
--- app/src/main/java/com/example/humpty/ActivitySocial.java
+++ app/src/main/java/com/example/humpty/ActivitySocial.java
@@ -2,29 +2,25 @@ package com.example.humpty;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.ActionBarOverlayLayout;
-import androidx.navigation.fragment.NavHostFragment;
 
-import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class ActivityHome extends AppCompatActivity {
+public class ActivitySocial extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_social);
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
 
         // Set Home selected
-        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setSelectedItemId(R.id.social);
 
         // Perform item selected listener
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,16 +33,15 @@ public class ActivityHome extends AppCompatActivity {
                         startActivity(new Intent(getApplicationContext(),ActivityEvents.class));
                         overridePendingTransition(0,0);
                         return true;
-                    case R.id.home:
-                        return true;
                     case R.id.social:
-                        startActivity(new Intent(getApplicationContext(),ActivitySocial.class));
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),ActivityHome.class));
                         overridePendingTransition(0,0);
                         return true;
                 }
                 return false;
             }
         });
-
     }
 }
