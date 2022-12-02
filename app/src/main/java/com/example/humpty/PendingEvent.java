151727028e552dc660cb304baf7163410ce148df
Gunnar Ortega <gorteg6@lsu.edu>
2022-12-02 00:28:15 -0600

    Font updates

---
+++ app/src/main/java/com/example/humpty/PendingEvent.java
@@ -0,0 +1,34 @@
+package com.example.humpty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class PendingEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pending_event);
+
+
+        final ImageButton button = (ImageButton) findViewById(R.id.buttonStartVoting);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),ActivityHome.class);
+                startActivity(i);
+
+            }
+        });
+    }
+
+
+
+}
